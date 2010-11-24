a1f85ea2aa836c25a9379190bd59f2fb6398ef0f
Tomas Vaisar <tomas@vaisar.cz>
2010-11-24 08:17:34 +0100

    - registrace feature do klienta - detekce feature u kontaktu - pokusy o poslani dummy zpravy

--- plugins/jabbim/xawa/xawa.py
+++ plugins/jabbim/xawa/xawa.py
@@ -11,16 +11,27 @@ from PyQt4 import QtCore, QtGui
 from PyQt4.QtWebKit import QWebView
 from urllib import quote, unquote
 from twisted.python import log
+from twisted.words.protocols.jabber.xmlstream import IQ
+from twisted.words.xish.domish import Element
 
 class LittleClass(QtCore.QObject):  
     def __init__(self,parent):
-        QtCore.QObject.__init__(self,parent)
+        QtCore.QObject.__init__(self,parent.window)
         self.setObjectName('ObjectAddedByPython')
         
     @QtCore.pyqtSlot(int, int, result=int)
     def scitej(self,a,b):
         return a+b
-
+    
+    @QtCore.pyqtSlot(result=str)
+    def printInfo(self):
+        return 'XAWA version 0.1' 
+    
+    @QtCore.pyqtSlot(result=str)
+    def sendDummyMessage(self):
+        #iq = IQ(self.parent().main.client.xmlstream, 'set')    
+        return self.parent.sendMessage('ahoj')
+    
 class config:
     def __init__(self,main):
         self.main = main
@@ -39,9 +50,12 @@ class Plugin(plugins.PluginBase):
         self.url = 'http://xawa.vaisar.cz'
         self.count = 0
         
-        if main:
+        if main:           
             self.loadConfig(homedir)
             self.loadConfig()
+            
+            self.registerHandler('on_authd', self.on_authd)
+            
             self.window = self.loadWindow("%s/xawaWindow_ui.py" % self.pluginDir, self.main)
             self.window.setWindowIcon(self.main.windowIcon())
 
@@ -59,10 +73,28 @@ class Plugin(plugins.PluginBase):
         else:
             self.loadConfig(homedir)
             
+    def on_authd(self):
+        try:
+            self.registerFeature("http://xawa.vaisar.cz/protocol/xawa")
+        except Exception, ex:
+            raise ex
+            
                         
     def buildMainWindowMenu(self):
         menu=self.mainWindowMenu()
         menu.addAction("Open XAWA Window", self.openWindow);
+    
+    def buildContactMenu(self,menu,contact):
+        jid = unicode(contact.jid)
+        
+        if self.main.client.hasFeature(jid,"http://xawa.vaisar.cz/protocol/xawa"):
+            # wheee, nekdo pouziva nasi feature!
+            
+            self.action = menu.addAction(self.tr("Open XAWA window"))
+            self.action.setData(QtCore.QVariant(jid))
+            self.action.setObjectName("xawa_menu_button")
+
+            QtCore.QObject.connect(self.action,QtCore.SIGNAL("triggered ( bool )"),self.openWindow)
     
     def on_message(self,msg):
         pass
@@ -85,7 +117,7 @@ class Plugin(plugins.PluginBase):
     
     def initJavascript(self):
         try:
-            self.window.ui.webView.page().mainFrame().addToJavaScriptWindowObject('PyLittleClass',LittleClass(self.window))
+            self.window.ui.webView.page().mainFrame().addToJavaScriptWindowObject('PyLittleClass',LittleClass(self))
         except Exception,ex:
             raise ex
             
@@ -96,3 +128,23 @@ class Plugin(plugins.PluginBase):
 
         except Exception, ax:
             raise ax
+        
+
+            
+    def sendMessage(self,message):
+            
+        #iq = IQ(self.parent().main.client.xmlstream, 'set')
+        result = 'ok'
+        try:
+            message = Element((None,'message'))
+            message['from'] = 'johntestovic@jabbim.cz/Jabbim'
+            message['to'] = 'incik@jabbim.cz/Lenicka'
+            body = message.addElement('body', None, 'Zdar!')
+         
+            self.main.client.xmlstream.send(message)
+            result = 'jo'
+        except Exception, ex:
+            result = ex;
+    
+        return result
+    
