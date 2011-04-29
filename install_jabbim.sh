ae804e9cf36c395c16fe58456199523e64bcd865
Tomáš Vaisar <incik@tardis.(none)>
2011-04-29 21:14:19 +0200

    - modified "app selector" - added script that install latest version of Jabbim and XAWA on Ubuntu   machine

---
+++ install_jabbim.sh
@@ -0,0 +1,6 @@
+sudo apt-get install subversion pyqt4-dev-tools python-configobj python-twisted-words
+svn co svn://dev.jabbim.cz/jabbim/trunk jabbim
+wget https://github.com/incik/XAWA/zipball/master --no-check-certificate -O xawa.zip
+unzip xawa.zip
+mkdir jabbim/src/plugins/xawa
+cp -r incik-XAWA-*/plugins/jabbim/xawa/ jabbim/src/plugins/
